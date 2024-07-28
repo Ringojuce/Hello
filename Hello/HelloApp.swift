0efd523815727b982c1b641df1ec5e987d19f85c
Ringojuce <sigo7100@gmail.com>
2024-07-28 13:09:13 +0900

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by miya on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
